531afb9ed9547a7535d1fe77bddc6d538db8f610
Michael Chang <michael.chang25@gmail.com>
2015-01-24 22:09:05 -0800

    Facebook Login Implemetnation

--- src/com/cse190/petcafe/MainActivity.java
+++ src/com/cse190/petcafe/MainActivity.java
@@ -1,18 +1,104 @@
 package com.cse190.petcafe;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.LoginButton.UserInfoChangedCallback;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+	
+	private LoginButton loginButton;
+	private UiLifecycleHelper uiHelper;
+	private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
+	private final String LOGTAG = "Pet Cafe";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        uiHelper = new UiLifecycleHelper(this, statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
+        
+        loginButton = (LoginButton)findViewById(R.id.fbLoginButton);
+        loginButton.setUserInfoChangedCallback(new UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                if (user != null) {
+                	Log.i(LOGTAG, "You are now logged in");
+                } else {
+                	Log.i(LOGTAG, "You are now not logged in");
+                }
+            }
+        });
+    }
+    
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state,
+                Exception exception) {
+            if (state.isOpened()) {
+                Log.i(LOGTAG, "Facebook session opened");
+            } else if (state.isClosed()) {
+                Log.i(LOGTAG, "Facebook session closed");
+            }
+        }
+    };
+    
+    public boolean checkPermissions() {
+        Session s = Session.getActiveSession();
+        if (s != null) {
+            return s.getPermissions().contains("publish_actions");
+        } else
+            return false;
     }
 
+    public void requestPermissions() {
+        Session s = Session.getActiveSession();
+        if (s != null)
+            s.requestNewPublishPermissions(new Session.NewPermissionsRequest(
+                    this, PERMISSIONS));
+    }
+    
+    @Override
+    public void onResume()
+    {
+    	super.onResume();
+        uiHelper.onResume();
+    }
+    
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        uiHelper.onPause();
+    }
+    
+    @Override
+    public void onDestroy()
+    {
+    	super.onDestroy();
+    	uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
+        uiHelper.onSaveInstanceState(savedState);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
