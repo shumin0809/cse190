a26514dd2e0230eb1ab651d8969c245f7056e409
shumin0809 <shumin0809@yahoo.com>
2015-02-17 10:58:33 -0800

    update

--- src/com/example/haha/MainActivity.java
+++ src/com/example/haha/MainActivity.java
@@ -100,13 +100,21 @@ public class MainActivity extends Activity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
         	Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
         	startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+			return true;
+		case R.id.action_logout:
+			logout();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+    }
+    
+    private void logout() {
+    	// do logout here
     }
 
     /**
