372fc6712381a8dd6282b5db5a3e203f1568f9ce
shumin0809 <shumin0809@yahoo.com>
2015-02-23 22:56:20 -0800

    initial friends list functionalities

--- src/com/example/haha/FindFriendsActivity.java
+++ src/com/example/haha/FindFriendsActivity.java
@@ -14,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,11 +37,11 @@ public class FindFriendsActivity extends Activity {
 	private LocationManager locationManager;
 
 	private Button retrieveLocationButton;
-	
+
 	private ListView friendsList;
-	
+
 	private FriendsAdapter friendsAdapter;
-	
+
 	private ArrayList<Friend> friends = new ArrayList<Friend>();
 
 	@Override
@@ -49,15 +51,11 @@ public class FindFriendsActivity extends Activity {
 
 		setContentView(R.layout.activity_find_friends);
 
-
 		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-		locationManager.requestLocationUpdates(
-				LocationManager.GPS_PROVIDER,
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
 				MINIMUM_TIME_BETWEEN_UPDATES,
-				MINIMUM_DISTANCE_CHANGE_FOR_UPDATES,
-				new MyLocationListener()
-				);
+				MINIMUM_DISTANCE_CHANGE_FOR_UPDATES, new MyLocationListener());
 
 		// testing current user location method, could be removed
 		retrieveLocationButton = (Button) findViewById(R.id.retrieve_location_button);
@@ -67,25 +65,25 @@ public class FindFriendsActivity extends Activity {
 				showCurrentLocation();
 			}
 		});
-		
+
 		// display friends that within 5 miles distance
-		
-		//friends = getFriendsFromServer();   => we will call from server
-		//currently, we hard coding the friends list 
+
+		// friends = getFriendsFromServer(); => we will call from server
+		// currently, we hard coding the friends list
 		Friend friend = new Friend(R.drawable.ic_launcher, "dude", "5 miles");
-        for (int i = 0; i < 4; i++)
-            friends.add(friend);
-		
-		showFriends(friends);	
+		for (int i = 0; i < 4; i++)
+			friends.add(friend);
+
+		showFriends(friends);
 
 	}
-	
-	/******** call this method to send current user location  **********/
+
+	/******** call this method to send current user location **********/
 	public void sendMyLocation() {
 		Location location = locationManager
 				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 		// int user_id = current_user.getId();
-		
+
 		// send(location.getLongitude(), location.getLatitude(), user_id);
 	}
 
@@ -156,68 +154,78 @@ public class FindFriendsActivity extends Activity {
 
 		}
 	}
-	
-	
+
 	/****************** Display nearly friends ********************/
-	
+
 	private class FriendsAdapter extends ArrayAdapter<Friend> {
-	    public FriendsAdapter(Context context, List<Friend> friends) {
-	        super(context, android.R.layout.simple_list_item_1, friends);
-	    }
-	    @Override
-	    public View getView(int position, View convertView, ViewGroup parent) {
-	        Log.i("FriendsAdapter", "getView");
+		public FriendsAdapter(Context context, List<Friend> friends) {
+			super(context, android.R.layout.simple_list_item_1, friends);
+		}
 
-	        Friend friend = getItem(position);
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Log.i("FriendsAdapter", "getView");
 
-	        if (convertView == null) {
-	            convertView = LayoutInflater.from(getContext()).inflate(
-	                    R.layout.friend_detail, parent, false);
-	        }
+			Friend friend = getItem(position);
 
-	        ImageView avatar = (ImageView) convertView.findViewById(R.id.profile_picture);
-	        TextView username = (TextView) convertView.findViewById(R.id.user_name);
-	        TextView distance = (TextView) convertView.findViewById(R.id.distance);
+			if (convertView == null) {
+				convertView = LayoutInflater.from(getContext()).inflate(
+						R.layout.friend_detail, parent, false);
+			}
 
-	        avatar.setImageResource(friend.getProfilePicture());
-	        username.setText(friend.getUserName());
-	        distance.setText(friend.getDistance());
+			ImageView avatar = (ImageView) convertView
+					.findViewById(R.id.profile_picture);
+			TextView username = (TextView) convertView
+					.findViewById(R.id.user_name);
+			TextView distance = (TextView) convertView
+					.findViewById(R.id.distance);
 
-	        return convertView;
-	    }
+			avatar.setImageResource(friend.getProfilePicture());
+			username.setText(friend.getUserName());
+			distance.setText(friend.getDistance());
+
+			return convertView;
+		}
 
 	}
-	
+
 	private class Friend {
-		
-	    private int profile_picture;
-	    private String username;
-	    private String distance;
 
-	    public Friend(int profile_picture,String username,
-	                              String distance) {
-	        this.profile_picture = profile_picture;
-	        this.username = username;
-	        this.distance = distance;
-	    }
+		private int profile_picture;
+		private String username;
+		private String distance;
 
-	    public int getProfilePicture() {
-	    	return this.profile_picture;
-	    }
+		public Friend(int profile_picture, String username, String distance) {
+			this.profile_picture = profile_picture;
+			this.username = username;
+			this.distance = distance;
+		}
 
-	    public String getUserName() {
-	        return this.username;
-	    }
+		public int getProfilePicture() {
+			return this.profile_picture;
+		}
 
-	    public String getDistance() {
-	        return this.distance;
-	    }
+		public String getUserName() {
+			return this.username;
+		}
+
+		public String getDistance() {
+			return this.distance;
+		}
 	}
-	
+
 	public void showFriends(ArrayList<Friend> friends) {
 		friendsList = (ListView) findViewById(R.id.friends_list);
 		friendsAdapter = new FriendsAdapter(this, friends);
 		friendsList.setAdapter(friendsAdapter);
+
+		friendsList.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO future
+			}
+		});
 	}
 
 }
