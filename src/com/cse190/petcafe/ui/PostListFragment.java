ebd00a3d2019620054c7e02b461bbd39d2dcc7f5
scottmo <scottscmo@gmail.com>
2015-02-25 17:44:12 -0800

    Able to download posts from DB and update UI. There's some bug with the header though.

--- src/com/cse190/petcafe/ui/PostListFragment.java
+++ src/com/cse190/petcafe/ui/PostListFragment.java
@@ -9,8 +9,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -23,30 +25,37 @@ import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.cse190.petcafe.ApplicationSingleton;
 import com.cse190.petcafe.GlobalStrings;
 import com.cse190.petcafe.Petcafe_api;
 import com.cse190.petcafe.R;
 import com.cse190.petcafe.adapter.ListViewAdapter;
+import com.quickblox.users.model.QBUser;
 
-public class PostListFragment extends ScrollTabHolderFragment implements
-        OnScrollListener {
+public class PostListFragment
+        extends ScrollTabHolderFragment implements OnScrollListener {
 
     // Types of post list
-    public final static String KEY_POST_TYPE = "posttype";
     public final static int MY_POSTS       = 0;
     public final static int FRIENDS_POSTS  = 1;
     public final static int FILTERED_POSTS = 2;
     public final static int TABBED_POSTS   = 3;
 
-    private static final String KEY_ARG = "args";
+    // keys for bundles
+    public final static String KEY_POST_TYPE = "posttype";
+    private static final String KEY_POSTLIST = "postlist";
+    private static final String KEY_POST = "post";
 
     private static final String [] POST_TYPES = {"Stories", "Tips", "News", "Wiki"};
 
     private static final Map<String, Integer> POST_RESOURCES;
     static {
         Map<String, Integer> resMap = new HashMap<String, Integer>();
-        resMap.put("cat", R.drawable.noun_1836);
-        resMap.put("dog", R.drawable.noun_2015);
+        resMap.put("Cat",    R.drawable.noun_1836);
+        resMap.put("Dog",    R.drawable.noun_2015);
+        resMap.put("Rabbit", R.drawable.noun_2015);
+        resMap.put("Pig",    R.drawable.noun_2015);
+        resMap.put("All",    R.drawable.noun_2015);
         POST_RESOURCES = Collections.unmodifiableMap(resMap);
     }
 
@@ -59,6 +68,10 @@ public class PostListFragment extends ScrollTabHolderFragment implements
     private int mPostListType;
     private String [] mRequestArgs;
     private int mOffset;
+    private JSONArray mPostJArr;
+    private ListViewAdapter mPostListAdapter;
+
+    private ProgressDialog mProgressDialog;
 
     /**
      * @param position
@@ -69,7 +82,7 @@ public class PostListFragment extends ScrollTabHolderFragment implements
         PostListFragment f = new PostListFragment();
         Bundle b = new Bundle();
         b.putInt(KEY_POST_TYPE, postListType);
-        b.putStringArray(KEY_ARG, new String[]{POST_TYPES[position]});
+        b.putStringArray(KEY_POSTLIST, new String[]{POST_TYPES[position]});
         f.setArguments(b);
         return f;
     }
@@ -82,7 +95,7 @@ public class PostListFragment extends ScrollTabHolderFragment implements
         PostListFragment f = new PostListFragment();
         Bundle b = new Bundle();
         b.putInt(KEY_POST_TYPE, postListType);
-        b.putStringArray(KEY_ARG, new String[]{searchText, tag, type});
+        b.putStringArray(KEY_POSTLIST, new String[]{searchText, tag, type});
         f.setArguments(b);
         return f;
     }
@@ -91,14 +104,13 @@ public class PostListFragment extends ScrollTabHolderFragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPostListType = getArguments().getInt(KEY_POST_TYPE);
-        mRequestArgs = getArguments().getStringArray("args");
+        mRequestArgs = getArguments().getStringArray(KEY_POSTLIST);
         mOffset = 0;
-
         mListItems = new ArrayList<ListViewItem>();
-        Resources resources = getResources();
 
         new GetPostListTask().execute(mPostListType, mRequestArgs);
-        // setListAdapter(new ListViewAdapter(getActivity(), mListItems));
+        Log.e("PostListFragment", "onCreate");
+        //setListAdapter(new ListViewAdapter(getActivity(), mListItems));
     }
 
     @Override
@@ -113,27 +125,29 @@ public class PostListFragment extends ScrollTabHolderFragment implements
         mListView.addHeaderView(placeHolderView);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1,
-                    int position, long arg3) {
-                // TODO Auto-generated method stub
-                Log.d("listitem", "clicked");
-                int itemPosition = position;
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Log.e("item", "Now position:" + position);
                 // String itemValue = (String)
-                // mListView.getItemAtPosition(position);
-                // Log.d("2", "2");
-                Intent intent;
-                switch (itemPosition) {
+                Object o = mListView.getItemAtPosition(position);
+                Log.e("mListView Object", o.toString());
+
+                switch (position) {
                 case 0:
                     break;
                 default:
-                    Log.d("item", "Now position:" + itemPosition);
-                    // bundle...
-                    intent = new Intent(getActivity(), ActivitySinglePost.class);
+                    Intent intent = new Intent(getActivity(), ActivitySinglePost.class);
+                    try {
+                        // bundle containg post info
+                        Bundle b = new Bundle();
+                        b.putString(KEY_POST,
+                                mPostJArr.getJSONObject(position - 1).toString());
+                        intent.putExtra(KEY_POST, b);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                     startActivity(intent);
-                    break;
                 }
             }
-
         });
         return v;
     }
@@ -145,7 +159,8 @@ public class PostListFragment extends ScrollTabHolderFragment implements
         mListView.setOnScrollListener(this);
         // mListView.setAdapter(new ArrayAdapter<String>(getActivity(),
         // R.layout.list_item, android.R.id.text1, mListItems));
-        mListView.setAdapter(new ListViewAdapter(getActivity(), mListItems));
+        mPostListAdapter = new ListViewAdapter(getActivity(), mListItems);
+        mListView.setAdapter(mPostListAdapter);
     }
 
     @Override
@@ -155,15 +170,14 @@ public class PostListFragment extends ScrollTabHolderFragment implements
         }
 
         mListView.setSelectionFromTop(1, scrollHeight);
-
     }
 
     @Override
     public void onScroll(AbsListView view, int firstVisibleItem,
             int visibleItemCount, int totalItemCount) {
         if (mScrollTabHolder != null)
-            mScrollTabHolder.onScroll(view, firstVisibleItem, visibleItemCount,
-                    totalItemCount, mPosition);
+            mScrollTabHolder.onScroll(view, firstVisibleItem,
+                    visibleItemCount, totalItemCount, mPosition);
     }
 
     @Override
@@ -176,10 +190,12 @@ public class PostListFragment extends ScrollTabHolderFragment implements
         @Override
         protected void onPreExecute() {
             // start spinning icon
+            mProgressDialog = ProgressDialog.show(getActivity(), "Loading", "Wait...");
         }
 
         @Override
         protected JSONArray doInBackground(Object... params) {
+            Log.e("PostListAsync", "doInBackground");
             int arrIndex = 0;
             JSONArray postArr = null;
             int postListType = (Integer)params[arrIndex++];
@@ -187,14 +203,17 @@ public class PostListFragment extends ScrollTabHolderFragment implements
             try {
                 if (postListType == MY_POSTS) {
                     postArr = api.getMyPost(args[0]);
+
                 } else if (postListType == FRIENDS_POSTS) {
                     postArr = api.getFriendsPost(args[0]);
+
                 } else if (postListType == FILTERED_POSTS) {
                     arrIndex = 0;
                     String title = args[arrIndex++];
                     String tag   = args[arrIndex++];
                     String type  = args[arrIndex++];
                     postArr = api.getPostByFilters(title, tag, type);
+
                 } else { // (postListType == TABBED_POSTS) || default
                     String type  = args[0];
                     postArr = api.getPostByFilters(null, null, type);
@@ -208,23 +227,37 @@ public class PostListFragment extends ScrollTabHolderFragment implements
 
         @Override
         protected void onPostExecute (JSONArray postArr) {
-            // turn off spinning icon
-
-            // errors
-            if (postArr == null) return;
-
-            // update UI
-            Resources resources = getResources();
-            for (int i = 0; i < postArr.length(); i++) {
-                try {
-                    JSONObject postObj = postArr.getJSONObject(i);
-                    int petIconRes = POST_RESOURCES.get(postObj.getString("tag"));
-                    mListItems.add(new ListViewItem(resources.getDrawable(petIconRes),
-                            getString(R.string.dog), postObj.getString("body")));
-                } catch (JSONException e) {
-                    Log.e(GlobalStrings.LOGTAG, e.toString());
-                }
-            }
+            Log.e("GetPostAsync", "onPostExecute");
+            mProgressDialog.dismiss(); // turn off spinning icon
+            updatePostList(postArr);   // update UI
         }
     }
+
+    // -----------------------------------------------------------
+    // Helper Functions
+    // -----------------------------------------------------------
+    private void updatePostList(JSONArray postArr) {
+        if (postArr == null) return;
+        // store post information
+        mPostJArr = postArr;
+        // fill up UI list
+        Resources resources = getResources();
+        for (int i = 0; i < postArr.length(); i++) {
+            try {
+                JSONObject postObj = postArr.getJSONObject(i);
+
+                Drawable petIconRes = resources.getDrawable(
+                        POST_RESOURCES.get(postObj.getString("tag")));
+                String title = postObj.getString("title");
+                String body = postObj.getString("body").substring(0, 50) + "...";
+
+                mListItems.add(new ListViewItem(petIconRes, title, body));
+            } catch (JSONException e) {
+                Log.e(GlobalStrings.LOGTAG, e.toString());
+            }
+        }
+        // update UI
+        mPostListAdapter.notifyDataSetChanged();
+    }
+
 }
